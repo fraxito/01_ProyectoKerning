a7c15f22c0c89103df5621c8d3f7235d741efb95
xp <xp@cetysMulti07>
2014-03-28 17:04:20 +0100

    He añadido la letra I

--- KerningConjunto/src/principal/VentanaKerningConjunto.java
+++ KerningConjunto/src/principal/VentanaKerningConjunto.java
@@ -74,6 +74,18 @@ boolean[][] letraV = {
     { false, false, false, false, true, false, false, false, false },
     { false, false, false, false, true, false, false, false, false },
 };
+boolean[][] letraI = {
+    { true, true, true, false, false, false, true, true, true },
+    { false, true, false, false, false, false, false, true, false },
+    { false, true, false, false, false, false, false, true, false },
+    { false, true, false, false, false, false, false, true, false },
+    { false, false, true, false, false, false, true, false, false }, 
+    { false, false, true, false, false, false, true, false, false },
+    { false, false, false, true, false, true, false, false, false },
+    { false, false, false, true, false, true, false, false, false },
+    { false, false, false, false, true, false, false, false, false },
+    { false, false, false, false, true, false, false, false, false },
+};
 
     /**
      * Creates new form VentanaKerningConjunto
