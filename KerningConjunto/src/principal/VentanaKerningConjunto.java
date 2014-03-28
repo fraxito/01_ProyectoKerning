3af4059cb603d4f392a1d8fd7fd06e5ec718e34a
xp <xp@cetysMulti14>
2014-03-28 15:08:47 +0100

    He añadido la letra I

--- KerningConjunto/src/principal/VentanaKerningConjunto.java
+++ KerningConjunto/src/principal/VentanaKerningConjunto.java
@@ -62,6 +62,19 @@ boolean[][] letraA = {
     { true, true, true, false, false, false, true, true, true }
 };
 
+boolean[][] letraI = {
+    { false, false, false, false, true, false, false, false, false },
+    { false, false, false, false, true, false, false, false, false },
+    { false, false, false, true, false, true, false, false, false },
+    { false, false, false, true, false, true, false, false, false },
+    { false, false, true, false, false, false, true, false, false },
+    { false, false, true, false, false, false, true, false, false },
+    { false, true, true, true, true, true, true, true, false },
+    { false, true, false, false, false, false, false, true, false },
+    { false, true, false, false, false, false, false, true, false },
+    { true, true, true, false, false, false, true, true, true }
+};
+
 boolean[][] letraV = {
     { true, true, true, false, false, false, true, true, true },
     { false, true, false, false, false, false, false, true, false },
